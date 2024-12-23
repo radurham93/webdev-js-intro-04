fe0152a2c1987d1b087777051949e368e43fefb5
Robert Durham <radurham93@gmail.com>
2024-12-23 11:55:49 -0500

    finished assignment

--- js/assignment.js
+++ js/assignment.js
@@ -1,17 +1,20 @@
 "use strict";
 
 // Create your references to the html elements here
-const ageInputEl = document.getElementById("age-input")
+const ageInputEl = document.getElementById("age-input");
 const submissionBtn = document.getElementById("submission-btn");
+const paragraph = document.getElementById("response");
 
 // create a mutable variable called age and do not assign it a value.
-
+ let age;
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
     
     // Write your code below
-
+if (age < 18) {paragraph.textContent = "You cannot vote or purchase alcohol.";}
+else if (age >= 18 && age < 21) {paragraph.textContent = "You can vote, but you cannot purchase alcohol.";}
+else {paragraph.textContent = "You can vote and purchase alcohol.";}
 }
 
 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+let healthPoints = 0;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else if (healthPoints <= 0) {console.log("Hero is dead");
+} else {
+    console.log("Hero is close to death.");
+}
+
+
+if (1===1) {console.log("Correct");}
+
+let age = 31;
+if (age >= 30) {console.log("You are old");}
+
+let score = 70;
+if (score >= 90){console.log("You got an A");}
+else if (score >= 80 && score < 90) {console.log("You got a B");}
+else if (score >= 70 && score < 80) {console.log("You got a C");}
+else if (score >= 60 && score < 70) {console.log("You got a D");}
+else {console.log("You failed")}
+
+let testScore = 50;
+if (testScore >= 60){console.log("You passed")}
+else {console.log("You failed")}
+*/
